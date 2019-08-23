0141b7f1a3b4dd3017129fcbc1e18c697a876904
shivavh123 <shivakumarvh9@gmail.com>
2019-08-23 06:23:04 +0530

    psutil_project

--- findtabs.py
+++ findtabs.py
@@ -1,35 +1,32 @@
+# program for getting openingtab&closingtab of application
+
 import psutil
 import datetime
-import threading
-import sys
 
 proc_list = []
-
 def opentab():
-    
     while True:
-            
         for proc in psutil.process_iter():
             try:
-                if proc.name() == 'idle':
-                        
+                if 'idle' in proc.name():
                     processName = proc.name()
                     processID = proc.pid
-                        
                     if processID not in proc_list:
                         print('alert: someone trying to enter your application...........')
-                        opening_t=datetime.datetime.today()
+                        opening_t=datetime.datetime.today()# to get time of openingtab
                         tm = "Started at {}".format(opening_t)
 
                         print(processName ,tm, ' with procees id: ', processID )
                         proc_list.append(processID)
-                            
-                           
-                    else:
-                        continue
-            except (psutil.NoSuchProcess, psutil.AccessDenied,):
-               pass
+                                
+                             
+                else:
+                    continue
+            except Exception as E:#(psutil.NoSuchProcess, psutil.AccessDenied,):
+               print(E)
+    
         
+    
 
             
 def closedtab():
@@ -45,61 +42,10 @@ def closedtab():
                 pass
 
 
-
-    
-                    
-##thread_list = []
-
-thread1=threading.Thread(target=opentab())
-##thread1.append(thread_list)
-
-
-thread2=threading.Thread(target=closedtab())
-##thread_list.append(thread2)
-thread1.start()
-thread2.start()
-
-thread1.join()
-
-
-##def run_my_app():
-##    for thread in thread_list:
-##        thread.start()
-##        print("Thread1 Started..")
-##def run_my_app1():
-##    for thread in thread_list:
-##        thread.join()
-##        print("Thread2 started..")
-##def run_main():
-##    run_my_app()
-##    run_my_app1()
-##    return "Done"
-##    
-##
-##if __name__ == '__main__':
-##    try:
-##        print("Starting Monitoring...")
-##        run_main()
-##    except Exception as Ex:
-##        print(Ex)
-##        sys.exit()
-##        
-##    
-##
-##
-##    
-
-
-
-
-
-##for thread in process:
-##    thread
-
-
-
-
                 
                 
                 
             
+if __name__ == '__main__':
+    opentab()
+    closedtab()
